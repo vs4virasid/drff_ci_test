d3e165ce2692149a468d03ee6eef08dcc8c18ac0
virasudhan <veerasudhan@meditechnos.com>
2025-11-11 19:09:54 +0530

    Initial commit for CI/CD test project

---
+++ app/tests.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+from .models import Item
+
+class ItemModelTest(TestCase):
+    def test_item_creation(self):
+        item = Item.objects.create(name="Test", description="CI/CD Test")
+        self.assertEqual(item.name, "Test")
